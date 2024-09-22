7e00b3103db4e54ee8bcf33d1590d1f35fc2c0b6
Dave <lapojapod@gmail.com>
2024-09-23 00:06:20 +0800

    Nesting components from foreign packages

--- app/app.go
+++ app/app.go
@@ -1,15 +1,16 @@
-package main
+package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"templ-tutorial/utils"
 
 	"github.com/a-h/templ"
 )
 
-func main() {
-
+func Start() {
 	logfile, err := os.OpenFile("error.log", os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Could not open log file: ", err)
@@ -17,11 +18,17 @@ func main() {
 	defer logfile.Close()
 
 	log.SetOutput(logfile)
-	pageComponent := page()
+
+	name := utils.RandomString(10)
+	fmt.Println(name)
+
+	pageComponent := Page()
 
 	http.Handle("/", templ.Handler(pageComponent))
 
 	log.Println("App running on http://localhost:8080")
+	// print to console
+	fmt.Println("App running on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("Could not start http server: ", err)
 	}
---
+++ main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "templ-tutorial/app"
+
+func main() {
+	app.Start()
+}
